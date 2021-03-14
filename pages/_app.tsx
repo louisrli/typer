0e333f6f83170cb65f8b125108a955f344956dc5
Louis Li <louisrli@gmail.com>
2021-03-14 22:16:38 +0000

    more lintingya

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
-import React, {FC} from 'react';
-import {AppProps} from 'next/app';
-import {wrapper} from '../lib/store';
+import React, { FC } from 'react';
+import { AppProps } from 'next/app';
+import { wrapper } from '../lib/store';
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
-
 );
 
 export default wrapper.withRedux(WrappedApp);
-
