6d96a046e0c7c4eca2e17947a3ffae8b63e4f11e
Louis Li <louisrli@gmail.com>
2021-03-16 03:59:27 +0000

    enableMapSet on immer.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from 'react';
 import { AppProps } from 'next/app';
+import { enableMapSet } from 'immer';
 import { wrapper } from '../lib/store';
+
+// I guess this is the entry point.
+// https://immerjs.github.io/immer/docs/installation
+enableMapSet();
 
 const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
