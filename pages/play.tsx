bc88f4a16cb5e2bf12a4b2a076a53c6d4b36b023
Louis Li <louisrli@gmail.com>
2021-03-17 03:34:26 +0000

    start parsing endpoint

--- pages/play.tsx
+++ pages/play.tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next';
 import React from 'react';
 import Layout from '../components/common/Layout';
 import PhraseSessionView from '../components/typing/PhraseSessionView';
+import { wrapper } from '../lib/store';
+import { PhraseData, typingActions } from '../reducers/typing';
 
 const Play: NextPage = () => {
   return (
@@ -10,5 +12,30 @@ const Play: NextPage = () => {
     </Layout>
   );
 };
+
+const OUR_JANK_DATABASE =
+  'https://gist.githubusercontent.com/louisrli/43a875b34c76c1cb80ad3f8b1bcef623/raw/1e6dba05dd7c599b6f73d2a89b473cd9cc3316fd/ru';
+export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {
+  // TODO(louisli): When we get a real server, figure out how this works.
+  const query = await fetch(OUR_JANK_DATABASE);
+  const json = await query.json();
+  const phrasePool: PhraseData[] = [];
+  for (const pd of json.phraseData) {
+    phrasePool.push({
+      phrase: pd.phrase,
+      // Prevent error:
+      // Reason: `undefined` cannot be serialized as JSON. Please use `null` or
+      // omit this value.
+      definitions: pd.definitions || null,
+      examples: !pd.examples
+        ? null
+        : pd.examples.map((ex: any) => {
+            return [ex.sourceExample?.text, ex.targetExample?.text];
+          }),
+    });
+  }
+
+  store.dispatch(typingActions.setPhrasePool(phrasePool));
+});
 
 export default Play;
