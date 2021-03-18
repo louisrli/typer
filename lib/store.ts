6af0cb14a25f09f5284e2df35cc14e1c2edb991e
Louis Li <louisrli@gmail.com>
2021-03-18 03:03:31 +0000

    redux-persist maybe working

--- lib/store.ts
+++ lib/store.ts
@@ -1,6 +1,8 @@
 import { combineReducers, createStore } from 'redux';
-import { MakeStore, createWrapper, Context, HYDRATE } from 'next-redux-wrapper';
+import { MakeStore, createWrapper, HYDRATE } from 'next-redux-wrapper';
 import { StateType } from 'typesafe-actions';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { typingReducer } from '../reducers/typing';
 import { statsReducer } from '../reducers/stats';
 import { settingsReducer } from '../reducers/settings';
@@ -26,8 +28,27 @@ const reducer = (state: RootState | undefined, action: any): RootState => {
 };
 
 // create a makeStore function
-const makeStore: MakeStore<RootState> = (context: Context) =>
-  createStore(reducer);
+const makeStore: MakeStore<RootState> = () => {
+  // https://github.com/fazlulkarimweb/with-next-redux-wrapper-redux-persist
+  if (typeof window === 'undefined') {
+    return createStore(reducer);
+  }
+
+  const persistConfig = {
+    key: 'nextjs',
+    whitelist: ['typing', 'settings', 'stats'],
+    storage, // if needed, use a safer storage
+  };
+
+  const persistedReducer = persistReducer(persistConfig, reducer);
+
+  const store = createStore(persistedReducer);
+
+  // eslint-disable-next-line
+  (store as any).__persistor = persistStore(store);
+
+  return store;
+};
 
 // export an assembled wrapper
 export const wrapper = createWrapper<RootState>(makeStore, { debug: true });
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,22 @@ import 'tailwindcss/tailwind.css';
 import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import { enableMapSet } from 'immer';
+import { PersistGate } from 'redux-persist/integration/react';
+import { useStore } from 'react-redux';
 import { wrapper } from '../lib/store';
 
 // I guess this is the entry point.
 // https://immerjs.github.io/immer/docs/installation
 enableMapSet();
 
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-);
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const store = useStore();
+  return (
+    // eslint-disable-next-line
+    <PersistGate persistor={( store as any).__persistor}>
+      <Component {...pageProps} />
+    </PersistGate>
+  );
+};
 
 export default wrapper.withRedux(WrappedApp);
